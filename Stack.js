262cf77bb7aadf28a15031208ef2331513dc00ec
Nicholas Zehr <nicholas.zehr@icloud.com>
2022-04-12 10:40:00 -0500

    Stack Commented.

--- Stack.js
+++ Stack.js
@@ -1,3 +1,4 @@
+// The Stack class allows us to store an array full of elements that we have put in the stack. We also have access to a copy of these methods when we instantiate each Stack using the keyword new. The methods we have used in this class are push, pop, peek, isEmpty, and printStack. This Stack class uses an array and array methods to execute it behavior, however we also get the added benefit of customizing specific behaviors such as print and our "Nothing else to pop!" statement.
 class Stack {
     constructor()
     {
