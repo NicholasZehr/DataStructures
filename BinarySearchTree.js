654b48cfcfa1585cd62c1b7b1635e9c1129e0afc
Nicholas Zehr <nicholas.zehr@icloud.com>
2022-04-11 22:45:58 -0500

    Finished commenting BST

--- BinarySearchTree.js
+++ BinarySearchTree.js
@@ -15,7 +15,12 @@ class BinarySearchTree {
     constructor() {
         this.root = null
     }
-
+// The insert method will take the value and create a new node with it.
+// it will then determine if the instance of this BST has a root. When 
+// no root is present it will store a reference to the newly create node 
+// in the root of this BST. If there is already a root reference present 
+// then we will call our helper function insertNode in order to sort the 
+// newly created node into it's apropiate position in the BST.     
     insert(value) {
         const newNode = new Node(value)
         if (this.root === null) {
@@ -25,6 +30,9 @@ class BinarySearchTree {
             this.insertNode(this.root, newNode)
         }
     }
+// insertNode is a helper function that we can recursively call in order
+// to sort a newly created node (newNode) into it's appropiate spot in our 
+// BST. The (root) of this function is simply whichever node we're evaluting in // order to position references to the newNode approptiately.
     insertNode(root, newNode) {
         if (newNode.value < root.value) {
             
